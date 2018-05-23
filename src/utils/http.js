ff61769b19a5cd54dea9033babd5a2f7bb3a5cfa
吴茂芳 <maofang.wu@lianshang.com>
2018-05-23 15:54:41 +0800

    fix

--- src/utils/http.js
+++ src/utils/http.js
@@ -49,7 +49,7 @@ class Http {
 					}
 					if(res.data.code == 200){
 						resolve(res.data.data || {});
-					}else if(res.data.code == -100 || res.data.code == -151){
+					}else if(res.data.code == -100 || res.data.code == -151 || res.data.code == -117){
 						var pages = getCurrentPages()    //获取加载的页面
 						var currentPage = pages[pages.length-1]    //获取当前页面的对象
 						var url = '/' + currentPage.route
