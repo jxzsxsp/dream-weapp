6a04fc9c48ce266da327e4f51255e845b54a520c
lianshang <lisa.huang@lianshang.com>
2018-05-24 12:38:02 +0800

    角标为0不展示

--- src/pages/mine/index/index.vue
+++ src/pages/mine/index/index.vue
@@ -18,7 +18,7 @@
       </div>
       <div class="order-tabs">
         <div class="index-order-item" v-for="(order, index) in orderStatus" :key="index" @click="navigateTo(order.href)">
-            <i v-if="(statusCount[index+1] || statusCount[index+1]==0)&&token" class="superscript">{{statusCount[index+1]}}</i>
+            <i v-if="(statusCount[index+1]) && token" class="superscript">{{statusCount[index+1]}}</i>
             <i class="iconfont" :class="order.class"></i>
             <div class="order-item-text">{{order.text}}</div>
         </div>
