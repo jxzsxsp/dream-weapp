af16bd4d52eeb3c42674f98ed68044ed2ef648ed
吴茂芳 <maofang.wu@lianshang.com>
2018-05-24 10:55:18 +0800

    fix

--- src/utils/http.js
+++ src/utils/http.js
@@ -51,12 +51,19 @@ class Http {
 					if(res.data.code == 200){
 						resolve(res.data.data || {});
 					}else if(res.data.code == -100 || res.data.code == -151 || res.data.code == -117){
-						var pages = getCurrentPages()    //获取加载的页面
-						var currentPage = pages[pages.length-1]    //获取当前页面的对象
-						var url = '/' + currentPage.route
-						wx.setStorageSync('loginToUrl', url);
-						wx.redirectTo({
-							url: '/pages/mine/login/main'
+						wx.showToast({
+							title: res.data.message,
+							icon: 'none',
+							mask: true,
+							success(){
+								var pages = getCurrentPages()    //获取加载的页面
+								var currentPage = pages[pages.length-1]    //获取当前页面的对象
+								var url = '/' + currentPage.route
+								wx.setStorageSync('loginToUrl', url);
+								wx.redirectTo({
+									url: '/pages/mine/login/main'
+								})
+							}
 						})
 					}else{
 						wx.showToast({
