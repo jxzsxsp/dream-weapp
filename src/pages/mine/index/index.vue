b21191547cac2e662355786d1d354b7bb9181f60
lianshang <lisa.huang@lianshang.com>
2018-05-24 19:18:59 +0800

    弹出授权

--- src/pages/mine/index/index.vue
+++ src/pages/mine/index/index.vue
@@ -180,6 +180,19 @@ export default {
   },
   created() {
     console.log("created");
+    wx.getSetting({
+      success(res) {
+          if (!res.authSetting['scope.record']) {
+              wx.authorize({
+                  scope: 'scope.record',
+                  success() {
+                      // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+                      wx.startRecord()
+                  }
+              })
+          }
+      }
+    })
   }
 };
 </script>
