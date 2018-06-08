7db9efb42f65a849cbad6f4eb30ae33099327bbb
lianshang <lisa.huang@lianshang.com>
2018-06-08 10:48:43 +0800

    update 如果城市返回为空，则文案显示定位失败

--- src/utils/publicHttpMethods.js
+++ src/utils/publicHttpMethods.js
@@ -7,6 +7,9 @@ export function convertLocation(latitude, longitude) {
   return new Promise((resolve, reject) => {
     http.otherGet('https://api.map.baidu.com/geocoder/v2/?ak=GBFGB6jgPlXXP7eVpbglmyST2Qya0jXE&location=' + latitude + ',' + longitude + '&output=json&pois=1', '', true, '')
     .then((opts) => {
+      if(opts.data.result.addressComponent.city==''){
+        opts.data.result.addressComponent.city = '定位失败'
+      }
       resolve({city: opts.data.result.addressComponent.city, latitude, longitude})
     }) 
   })
