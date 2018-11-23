c8d753c9ba2c490d936491b37019ffdc035eefe7
Sean.Xiao <jxzsxsp@qq.com>
2018-11-23 18:44:29 +0800

    finish order list component and use vant components

--- pages/order-list/order-list.js
+++ pages/order-list/order-list.js
@@ -5,7 +5,231 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: [
+      {
+        title: "梭织面料打卷/自检",
+        statusName: "待入库",
+        rollSteps: [
+          {
+            title: "共6卷"
+          }
+        ],
+        checkHouse: "绍兴交易园店",
+        logisticsTypeName: "代发货至收货地址",
+        steps: {
+          active: 1,
+          list: [
+            {
+              title: "下单成功"
+            },
+            {
+              title: "等待收货"
+            }
+          ]
+        },
+        showCancelBtn: true,
+        showCheckBtn: false,
+        showPayBtn: false
+      },
+      {
+        title: "梭织面料打卷/自检",
+        statusName: "已入库",
+        rollSteps: [
+          {
+            title: "共6卷"
+          },
+          {
+            title: "入库2卷"
+          }
+        ],
+        checkHouse: "绍兴交易园店",
+        logisticsTypeName: "代发货至收货地址",
+        steps: {
+          active: 2,
+          list: [
+            {
+              title: "下单成功"
+            },
+            {
+              title: "已收货"
+            },
+            {
+              title: "等待验布"
+            }
+          ]
+        },
+        showCancelBtn: false,
+        showCheckBtn: false,
+        showPayBtn: false
+      },
+      {
+        title: "梭织面料打卷/自检",
+        statusName: "验布中",
+        rollSteps: [
+          {
+            title: "共6卷"
+          },
+          {
+            title: "入库2卷"
+          },
+          {
+            title: "验布2卷"
+          }
+        ],
+        checkHouse: "绍兴交易园店",
+        logisticsTypeName: "代发货至收货地址",
+        steps: {
+          active: 2,
+          list: [
+            {
+              title: "已收货"
+            },
+            {
+              title: "验布中"
+            },
+            {
+              title: "待付款"
+            }
+          ]
+        },
+        showCancelBtn: false,
+        showCheckBtn: true,
+        showPayBtn: false
+      },
+      {
+        title: "梭织面料打卷/自检",
+        statusName: "验布完成",
+        rollSteps: [
+          {
+            title: "共6卷"
+          },
+          {
+            title: "入库6卷"
+          },
+          {
+            title: "验布6卷"
+          }
+        ],
+        checkHouse: "绍兴交易园店",
+        logisticsTypeName: "代发货至收货地址",
+        steps: {
+          active: 2,
+          list: [
+            {
+              title: "已收货"
+            },
+            {
+              title: "验布完成"
+            },
+            {
+              title: "待付款"
+            }
+          ]
+        },
+        showCancelBtn: false,
+        showCheckBtn: true,
+        showPayBtn: false
+      },
+      {
+        title: "梭织面料打卷/自检",
+        statusName: "待付款",
+        rollSteps: [
+          {
+            title: "共6卷"
+          },
+          {
+            title: "入库6卷"
+          },
+          {
+            title: "验布6卷"
+          }
+        ],
+        checkHouse: "绍兴交易园店",
+        logisticsTypeName: "代发货至收货地址",
+        steps: {
+          active: 2,
+          list: [
+            {
+              title: "验布完成"
+            },
+            {
+              title: "待付款"
+            },
+            {
+              title: "待出库"
+            }
+          ]
+        },
+        showCancelBtn: false,
+        showCheckBtn: true,
+        showPayBtn: true
+      },
+      {
+        title: "梭织面料打卷/自检",
+        statusName: "待出库",
+        rollSteps: [
+          {
+            title: "共6卷"
+          },
+          {
+            title: "入库6卷"
+          },
+          {
+            title: "验布6卷"
+          }
+        ],
+        checkHouse: "绍兴交易园店",
+        logisticsTypeName: "代发货至收货地址",
+        steps: {
+          active: 2,
+          list: [
+            {
+              title: "待付款"
+            },
+            {
+              title: "待出库"
+            },
+            {
+              title: "已完成"
+            }
+          ]
+        },
+        showCancelBtn: false,
+        showCheckBtn: true,
+        showPayBtn: false
+      },
+      {
+        title: "梭织面料打卷/自检",
+        statusName: "已出库",
+        rollSteps: [
+          {
+            title: "共6卷"
+          },
+          {
+            title: "入库6卷"
+          },
+          {
+            title: "验布6卷"
+          }
+        ],
+        checkHouse: "绍兴交易园店",
+        logisticsTypeName: "代发货至收货地址",
+        steps: {
+          active: 2,
+          list: [
+            {
+              title: "待出库"
+            },
+            {
+              title: "已完成"
+            }
+          ]
+        },
+        showCancelBtn: false,
+        showCheckBtn: true,
+        showPayBtn: false
+      }
+    ]
   },
 
   /**
