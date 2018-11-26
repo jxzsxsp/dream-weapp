dfece7f784d3c4edcc827a4b5d791e53872e536c
zhang759740844 <759740844@qq.com>
2018-11-26 13:46:56 +0800

    update  下单页面

--- pages/place-order/place-order.js
+++ pages/place-order/place-order.js
@@ -1,12 +1,51 @@
 import {$Page, $wx} from '../../genji4mp/index'
+import {http, urls} from '../../net/index'
 
 const data = {
+  // 收货人
+  customerDetail: null,
+
+  selectedStore: {},
+  selectedFabricType: {},
+  selectedCheckType: {},
+  selectedLogisticType: {},
+  buyerMessage: '123',
+  priceDetail: {},
+  showPriceDetail: false,
+
+  clothLength: 0,
+  volumeNumber: 1,
+
+  fabricTypeList: [{id: 1, name:"123"}, {id: 2, name: "332"}],
+}
+
+const lifeCycle = {
+  onLoad: function () {
+    
+  }
 }
 
 const viewAction = {
-  test: function (t, v) {
+  selectStore: function () {
+  },
+  selectFabric: function (d, v) {
+  },
+  selectedCheckType: function () {
+
+  },
+  selectLogistic: function () {
+  },
+  finishInput: function (d, v) {
+    console.log(d)
+    console.log(v)
+  },
+}
+
+const privateMethod = {
+  volumeNumberChange: function (d) {
+    console.log(d.detail)
   }
 }
 
 
-$Page(null, data, null, null, viewAction)
+$Page(null, data, lifeCycle, privateMethod, viewAction)
