714921725e3e705c93bd69842c07df117d980eb8
zhang759740844 <759740844@qq.com>
2018-11-26 14:03:04 +0800

    update 增加url

--- net/urls/main.js
+++ net/urls/main.js
@@ -1,3 +1,22 @@
 export default {
-  bannerList: '/public/banner-resources'
+  // 订单初始化
+  orderInit: '/order/init',
+  // 提交订单
+  orderSubmit: '/order/submit',
+  // 订单价格
+  orderPrice: '/price/calc-proposed-price',
+  // 获取地址类型
+  addressType: '/customer-address/type-list',
+  // 地址列表
+  addressList: '/customer-address/list',
+  // 增加地址
+  addressAdd: '/customer-address/add',
+  // 修改地址
+  addressUpdate: '/customer-address/update',
+  // 删除地址
+  addressDelete: '/customer-address/delete',
+  // 设置默认地址
+  addressDefault: '/customer-address/default',
+  // 获取地址详情
+  addressDetail: '/customer-address/detail',
 }
