2c7445dbeb47dd87750796d2d40b7acd7deffbfe
zhang759740844 <759740844@qq.com>
2018-11-26 17:23:30 +0800

    update 下单页面逻辑

--- net/mock.js
+++ net/mock.js
@@ -1,6 +1,74 @@
 import urls from './urls/index'
 
+const orderInit = {
+  [urls.orderInit]: {
+    "storeList": [{
+      "id": 1,
+      "name": "绍兴万商路店1",
+      "address": "j上海市晋安区55路55弄j"
+    },{
+      "id": 2,
+      "name": "绍兴万商路店2",
+      "address": "j上海市晋安区55路55弄j"
+    },{
+      "id": 3,
+      "name": "绍兴万商路店3",
+      "address": "j上海市晋安区55路55弄j"
+    }],
+    "defaultCustomerAddress": {
+      "id": 1,
+      "contacts": "张成辉",
+      "contactMobile": "16602194415",
+      "detailAddress": "上海市闵行区打呼噜,上海市闵行区打呼噜,上海市闵行区打呼噜,上海市闵行区打呼噜,上海市闵行区打呼噜,上海市闵行区打呼噜"
+    },
+    "fabricTypeList": [{
+      "id": 1,
+      "name": "针织",
+      "unit": "公斤"
+    },{
+      "id": 2,
+      "name": "梭织",
+      "unit": "米"
+    }], //面料类型
+    "clothTypeList": [{
+      "id": 1,
+      "name": "高级验布",
+      "item": "高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布,高级验布"
+    },{
+      "id": 2,
+      "name": "中级验布",
+      "item": "中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布,中级验布"
+    },{
+      "id": 3,
+      "name": "初级验布",
+      "item": "初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布,初级验布"
+    }], //验布方式
+    "logisticsTypeList": [{
+      "id": 1,
+      "name": "用户自提"
+    },{
+      "id": 2,
+      "name": "上梦送货"
+    },{
+      "id": 3,
+      "name": "顺丰速递"
+    }] //取货方式
+  }
+}
+
+const calc = {
+  [urls.orderPrice]: {
+    "clothPriceDescription":"0.1元/m", //定价描述
+    "proposedPriceDescription":"￥110.00", //参考价格描述
+    "length":"100m", //数量 描述
+    "customerDiscountDescription":"9折", //优惠VIP描述
+    "discountPriceDescription":"￥11.00",//参考优惠价格描述
+    "waitPayProposedPriceDescription":"￥99.00",//待支付参考价格描述
+  }
+}
 
 
 export default {
+  ...orderInit,
+  ...calc
 }
--- pages/place-order/place-order.js
+++ pages/place-order/place-order.js
@@ -1,51 +1,178 @@
 import {$Page, $wx} from '../../genji4mp/index'
 import {http, urls} from '../../net/index'
 
+const props = {
+  STORE: 'selectStore',
+  CHECKTYPE: 'checkType',
+  LOGISTICTYPE: 'logisticType',
+  CLOTHLENGTH: 'clothLength',
+  VOLUMENUMBER: 'volumeNumber',
+}
+
 const data = {
   // 收货人
-  customerDetail: null,
+  customerDetail: {},
+  // 验布坊类表
+  storeList: [],
+  // 面料类型
+  fabricTypeList: [],
+  // 验布方式
+  checkTypeList: [],
+  // 取货方式
+  logisticsTypeList: [],
 
+  // 选中的验布坊
   selectedStore: {},
+  // 选中的验布方式
+  selectedCheckType: {}, 
+  // 选中的验布方式
   selectedFabricType: {},
-  selectedCheckType: {},
-  selectedLogisticType: {},
-  buyerMessage: '123',
-  priceDetail: {},
-  showPriceDetail: false,
-
+  // 验布长度
   clothLength: 0,
+  // 验布卷数
   volumeNumber: 1,
+  // 取货方式
+  selectedLogisticType: {},
 
-  fabricTypeList: [{id: 1, name:"123"}, {id: 2, name: "332"}],
+  // 价格详情
+  priceDetail: {},
+
+  buyerMessage: '123',
 }
 
 const lifeCycle = {
-  onLoad: function () {
-    
+  //TODO: 这个要告诉水平过来的时候要传一个验布方式的 id参数
+  onLoad: function (query) {
+    let data = this.data
+    http.get(urls.orderInit, {mock: true}).then(res => {
+      data.customerDetail = res.defaultCustomerAddress || {}
+      data.storeList = res.storeList
+      data.fabricTypeList = res.fabricTypeList
+      data.checkTypeList = res.clothTypeList
+      data.logisticsTypeList = res.logisticsTypeList
+    }).then(() => {
+      let checkTypeIndex = parseInt(data.checkTypeList[0].id)
+      // 找到选中的验布方式
+      data.checkTypeList.forEach((checkType, index) => {
+        if (checkType.id === parseInt(query.id)) {
+          checkTypeIndex = index
+        }
+      });
+      this.setData({
+        customerDetail: data.customerDetail,
+        fabricTypeList: data.fabricTypeList,
+        selectedCheckType: data.checkTypeList[checkTypeIndex]
+      })
+    })
   }
 }
 
 const viewAction = {
+  // 选择验布坊
   selectStore: function () {
+    let actionList = this.data.storeList.map((store, index) => {
+      return Object.assign({}, store, {type: this.props.STORE, index})
+    })
+    this.openActionList(actionList)
   },
+  // 选择面料类型
   selectFabric: function (d, v) {
+    this.data.selectedFabricType = v
+    this.calcPrice()
   },
-  selectedCheckType: function () {
-
+  // 选择验布方式
+  selectCheckType: function () {
+    let actionList = this.data.checkTypeList.map((checkType, index) => {
+      return Object.assign({}, checkType, {type: this.props.CHECKTYPE, index})
+    })
+    this.openActionList(actionList)
   },
+  // 选择取货方式
   selectLogistic: function () {
+    let actionList = this.data.logisticsTypeList.map((logisticType, index) => {
+      return Object.assign({}, logisticType, {type: this.props.LOGISTICTYPE, index})
+    })
+    this.openActionList(actionList)
   },
+  selectPriceDetail: function () {
+    this.setData({
+      showPriceDetail: true
+    })
+  },
+  // 结束输入
   finishInput: function (d, v) {
-    console.log(d)
-    console.log(v)
+    if (d.type === this.props.CLOTHLENGTH) {
+      this.setData({
+        clothLength: v
+      })
+      this.calcPrice()
+    } else if (d.type === this.props.VOLUMENUMBER) {
+      this.setData({
+        volumeNumber: v
+      })
+    }
+  },
+  // 选中弹出框
+  onActionSelected: function (d, v) {
+    switch (v.type) {
+      case this.props.STORE:
+        this.setData({
+          selectedStore: this.data.storeList[v.index],
+        })
+        break;
+      case this.props.CHECKTYPE:
+        this.setData({
+          selectedCheckType: this.data.checkTypeList[v.index]
+        })
+        this.calcPrice()
+        break;
+      case this.props.LOGISTICTYPE:
+        this.setData({
+          selectedLogisticType: this.data.logisticsTypeList[v.index]
+        })
+        break;
+      default:
+        break;
+    }
+    this.closeAllPopup()
   },
 }
 
 const privateMethod = {
-  volumeNumberChange: function (d) {
-    console.log(d.detail)
-  }
+  // 计算价格
+  calcPrice: function () {
+    const data = this.data
+    // 同时存在面料类型验布方式和米数后计算
+    if (data.selectedCheckType.id && data.selectedFabricType.id && data.clothLength) {
+      const params = {
+        fabricMeters: data.clothLength,
+        fabricType: data.selectedFabricType.id,
+        unit: data.selectedFabricType.unit,
+        clothType: data.selectedCheckType.id,
+        mock: true
+      }
+      http.post(urls.orderPrice, params).then(res => {
+        this.setData({
+          priceDetail: res
+        })
+      })
+    }
+  },
+  // 打开底部弹框
+  openActionList: function (actionList) {
+    this.setData({
+      actionList,
+      showActionList: true
+    })
+  },
+  // 关闭底部弹框
+  closeAllPopup: function () {
+    this.setData({
+      showActionList: false,
+      showPriceDetail: false
+    })
+  },
 }
 
 
-$Page(null, data, lifeCycle, privateMethod, viewAction)
+$Page(props, data, lifeCycle, privateMethod, viewAction)
