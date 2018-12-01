cda79d5ceb9fdf5afb5883b5fe86713d6d53d8e9
zhang759740844 <759740844@qq.com>
2018-12-01 14:42:40 +0800

    add util类

---
+++ utils/index.js
@@ -0,0 +1,5 @@
+import * as jshelper from './jshelper'
+
+export default {
+  ...jshelper
+}
---
+++ utils/jshelper.js
@@ -0,0 +1,24 @@
+/**
+ * 判断传入的是否是一个对象
+ *
+ * @export
+ * @param {Object} obj 待确认对象
+ * @returns Bool
+ */
+export function isObject (obj) {
+  return typeof(obj) === 'object'
+}
+
+/**
+ * 判断是否是一个空对象
+ *
+ * @export
+ * @param {Object} obj
+ * @returns
+ */
+export function isEmptyObject (obj) {
+  if (!isObject(obj)) {
+    return false
+  }
+  return JSON.stringify(obj) === '{}'
+}
