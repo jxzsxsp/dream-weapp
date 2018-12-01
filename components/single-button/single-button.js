9d1d11af65b3cea63d6e93090f9984998c033715
zhang759740844 <759740844@qq.com>
2018-12-01 17:18:27 +0800

    update 单选组件逻辑

--- components/single-button/single-button.js
+++ components/single-button/single-button.js
@@ -1,3 +1,19 @@
+/*
+  单选组件
+
+  数据数组,必须包含 name 和 id
+  datas: [{
+    name: 'xxx',
+    id: 123,
+  }]
+
+  初始化选中的 id
+  defaultSelectedId: 999
+
+  选中的数据的回调
+  bind:clickcallback
+
+ */
 Component({
   data: {
     selectedIndex: 999
@@ -7,6 +23,20 @@ Component({
       type: Array,
       value: []
     },
+    defaultSelectedId: {
+      type: Number,
+      value: 999
+    }
+  },
+  ready: function () {
+    this.data.datas.forEach((element, index) => {
+      if (element.id === this.data.defaultSelectedId) {
+        let selectedIndex = index
+        this.setData({
+          selectedIndex
+        })
+      }
+    });
   },
   methods: {
     onTap: function (value) {
