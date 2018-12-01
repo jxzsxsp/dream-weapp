623c50b47f3a136ad798d170ee917c52fa1547ca
zhang759740844 <759740844@qq.com>
2018-12-01 14:42:58 +0800

    update 地址列表

--- pages/address-add/address-add.js
+++ pages/address-add/address-add.js
@@ -1,15 +1,44 @@
 import { $Page } from '../../genji4mp/index'
+import constants  from '../../constants/index'
+import utils from '../../utils/index'
 
 const data = {
-
+  contacts: '',
+  contactMobile: '',
+  district: '',
+  address: '',
+  addressType: 0,
+  defaultAddress: false,
 }
 
 const lifeCycle = {
-
+  onLoad: function (query) {
+    // 修改地址
+    !utils.isEmptyObject(query) && this.setData({
+      contacts: query.contracts,
+      contactMobile: query.contactMobile,
+      district: query.district,
+      address: query.address,
+      addressType: query.addressType,
+      defaultAddress: query.defaultAddress
+    })
+  }
 }
 
 const viewAction = {
-
+  switchChange (d, v) {
+    console.log(v)
+  },
+  finishInput (d, v) {
+    switch (d.type) {
+      case 'contacts':
+        this.setData({})
+        break;
+    
+      default:
+        break;
+    }
+  }
 }
 
 const privateMethod = {
