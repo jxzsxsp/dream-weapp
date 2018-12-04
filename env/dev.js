9e8f26b4fc36be9a2d108c00cdc15c4ae4548a44
zhang759740844 <759740844@qq.com>
2018-12-04 11:31:43 +0800

    update 设置环境

--- env/dev.js
+++ env/dev.js
@@ -1,14 +1,7 @@
 export const isOnline = false
 
-// export default {
-//   Login: 'http://172.16.2.166:8080',
-//   url: 'http://chameleon-server.dev.lsfash.cn',  // 主域名
-//   Pantone: 'http://pantone-server.dev.lsfash.cn'
-
-// }
 
 export default {
   Login: 'https://sso2.lian-shang.cn',
-  url: 'https://chameleon.lian-shang.cn',  //主域名
-  Pantone: 'https://colorlab.lian-shang.cn'
+  url: 'http://172.16.2.157:8080/',  //主域名
 }
