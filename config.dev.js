cc838c8441d50fa7c55d2886a14627fbc35201c8
Sean.Xiao <jxzsxsp@qq.com>
2018-12-14 10:53:40 +0800

    fix

--- config.dev.js
+++ config.dev.js
@@ -3,7 +3,7 @@ const config = {
   app_id: 6,
   domain_name: 'somersault-cloud-api.lian-shang.cn',
   sso_url: 'http://172.16.2.166:8080',
-  base_url: 'http://172.16.1.139:8080',
+  base_url: 'http://172.16.2.156:8080',
   pay_url: ''
 }
 
