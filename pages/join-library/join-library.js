fb883ee08597586dc20ee70319d9481f36132b86
Sean.Xiao <jxzsxsp@qq.com>
2018-12-21 19:49:32 +0800

    完成加入颜色库UI

--- pages/join-library/join-library.js
+++ pages/join-library/join-library.js
@@ -6,78 +6,40 @@ const props = {
 }
 
 const data = {
-  maxTitleTextNum: 8,
-  maxDescTextNum: 140,
-  canSave: false,
-  id: 0,
-  name: '',
-  desc: '',
+  colorLibraryList: [],
+  defaultColorLibrary: {},
 }
 
 const lifecycle = {
   onLoad: function (query) {
+    this.getColorLibraryList().then(res => {
+      console.log(res)
+
+      this.setData({
+        colorLibraryList: res
+      })
+
+      for(let i = 0; i < res.length; i++) {
+        if(res[i].type === 0) {
+          this.setData({
+            defaultColorLibrary: res[i]
+          })
+          break
+        }
+      }
+    })
   },
   onShow: function () {
-    this.checkCanSave()
   }
 }
 
 const viewAction = {
-  save: function () {
-
-    if (this.data.canSave) {
-      this.createColorLibrary().then(res => {
-        console.log(res)
-      })
-    }
-
-  },
-  nameChange: function (d, v) {
-
-    this.setData({
-      name: v
-    })
-
-    this.checkCanSave()
-
-  },
-  descChange: function (d, v) {
-
-    this.setData({
-      desc: v
-    })
-
-    this.checkCanSave()
-
-  },
 }
 
 const privateMethods = {
-  isNotEmpty: function (str) {
-    return !!str && str.length > 0
-  },
-  checkCanSave: function () {
-    if (this.isNotEmpty(this.data.name) && this.isNotEmpty(this.data.desc)) {
-      this.setData({
-        canSave: true
-      })
-    } else {
-      this.setData({
-        canSave: false
-      })
-    }
-  },
   getColorLibraryList: function () {
     return http.getList(urls.colorLibraryList, this.props.loadingState, {
       mock: true,
-    })
-  },
-  createColorLibrary: function () {
-    return http.post(urls.colorLibrarySave, {
-      mock: true,
-      id: this.data.id,
-      name: this.data.name,
-      description: this.data.desc
     })
   },
 }
