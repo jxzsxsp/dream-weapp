9dd60b6da3fe0aceb3666ed4427cca11579cd3ba
潘金龙 <992477019@qq.com>
2018-12-26 10:59:34 +0800

    我的团队滚动加载异常修复

--- pages/SubMembers/SubMembers.js
+++ pages/SubMembers/SubMembers.js
@@ -33,29 +33,45 @@ Page({
     },
     // onShareAppMessage: function() {},
     loadData: function(t, n) {
+        var tm = this;
         wx.showLoading({
             title: "加载中"
         }), a.getOpenId(function(o) {
             wx.request({
                 url: a.getUrl("SubMembers"),
                 data: {
-                    openId: o,
+                    // openId: o,
+                    openId: 'o_rWK5YTqOJ2ruCGdsjZn4YJ8ovI',
                     pageIndex: t.data.PageIndex,
                     pageSize: t.data.PageSize
                 },
                 success: function(a) {
+
+                    if (!tm.data.subMemberList && a.data.SubMember_get_response.SubMembers.length == 0) return;
+
                     if (void 0 == a.data.error_response) {
                         var o = a.data.SubMember_get_response, r = o.SubMembers;
+                        console.log(n)
                         if (n) {
-                            var s = t.data.subMemberData;
-                            s.push.apply(s, r), t.setData({
+                            // var s = t.data.subMemberData;
+                            // s.push.apply(s, r), t.setData({
+                            //     subMemberList: s,
+                            //     ExpandMemberInMonth: t.data.ExpandMemberInMonth,
+                            //     ExpandMemberAll: t.data.ExpandMemberAll,
+                            //     LowerUserSaleTotal: t.data.LowerUserSaleTotal
+                            // });
+                            var s = t.data.subMemberList.concat(r)
+                            t.setData({
                                 subMemberList: s,
                                 ExpandMemberInMonth: t.data.ExpandMemberInMonth,
                                 ExpandMemberAll: t.data.ExpandMemberAll,
                                 LowerUserSaleTotal: t.data.LowerUserSaleTotal
                             });
+
                         } else {
                             r.Total;
+                            console.log(r)
+                            console.log(r.Total)
                             t.setData({
                                 subMemberList: r,
                                 isEmpty: t.data.isempty,
