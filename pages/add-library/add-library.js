18c12ff0ceb5c4548de6991ffdb26f820dfae621
zhang759740844 <759740844@qq.com>
2018-12-26 15:45:13 +0800

    refactor 添加颜色库

--- pages/add-library/add-library.js
+++ pages/add-library/add-library.js
@@ -1,15 +1,18 @@
 import { $wx, $Page } from '../../genji4mp/index'
 import { http, urls } from '../../net/index'
 import constant from '../../constant/index'
+import utils from '../../utils/index'
 
 const props = {
+  // 编辑颜色库的类型
+  type: null,
+  libraryDetail: {},
+  libraryColorIdList: [],
 }
 
 const data = {
-  maxTitleTextNum: 8,
-  maxDescTextNum: 140,
   canSave: false,
-  id: '',
+  libraryId: '',
   name: '',
   desc: '',
   libraryDetail: {},
@@ -17,31 +20,23 @@ const data = {
 
 const lifecycle = {
   onLoad: function (query) {
-    console.log(query)
-
-    this.setData({
-      ...query,
-    })
-
-    if (query.libraryDetail && query.libraryDetail.name) {
+    if (!utils.isEmptyObject(query)) {
+      this.props.type = query.type
+      this.props.libraryDetail = query.libraryDetail
+      // 除了分享跳到该界面，其他都是编辑颜色库
       if (query.type !== constant.ColorLibraryActionType.SaveLibrary) {
-        if (query.libraryDetail.id > 0) {
-          $wx.setNavigationBarTitle({
-            title: '编辑颜色库',
-          })
-        }
-        
-        this.setData({
-          id: query.libraryDetail.id,
-          name: query.libraryDetail.name,
-          desc: query.libraryDetail.description,
+        $wx.setNavigationBarTitle({
+          title: '编辑颜色库'
         })
-      } else {
         this.setData({
-          name: query.libraryDetail.name,
-          desc: query.libraryDetail.description,
-        })
+          libraryId: query.libraryDetail.id
+        }) 
       }
+      this.setData({
+        name: query.libraryDetail.name,
+        desc: query.libraryDetail.description,
+        libraryColorIdList: query.libraryColorIdList || []
+      })
     }
   },
   onShow: function() {
@@ -51,64 +46,38 @@ const lifecycle = {
 
 const viewAction = {
   save: function () {
-
-    if(this.data.canSave) {
-      this.createColorLibrary().then(res => {
-        console.log(res)
-        this.setData({
-          libraryId: res.data
-        })
-        
-        let libraryDetail = this.data.libraryDetail
-        libraryDetail.id = res.data
-        libraryDetail.name = this.data.name
-        libraryDetail.description = this.data.desc
-
-        if (this.data.type === constant.ColorLibraryActionType.Move_Single
-          || this.data.type === constant.ColorLibraryActionType.Move_Multiple) {
-          this.moveColorToLibrary().then(res => {
-            $wx.navigateBack(2, {
-              type: this.data.type,
-              libraryDetail: libraryDetail
-            })
-          }, '已移动到 ' + libraryDetail.name)
-        } else if (this.data.type === constant.ColorLibraryActionType.Add_Single
-          || this.data.type === constant.ColorLibraryActionType.Add_Multiple
-          || this.data.type === constant.ColorLibraryActionType.SaveColor
-          ) {
-          this.addColorToLibrary().then(res => {
-            $wx.navigateBack(2, {
-              type: this.data.type,
-              libraryDetail: libraryDetail
-            }, '已加入到 ' + libraryDetail.name)
-          })
-        } else {
-          $wx.navigateBack(1, {
-            type: this.data.type,
-            libraryDetail: libraryDetail
-          })
-        }
+    this.createColorLibrary().then(res => {
+      this.setData({
+        libraryId: res.data
       })
-    }
 
+      if (this.props.type === constant.ColorLibraryActionType.Move_Single
+        || this.props.type === constant.ColorLibraryActionType.Move_Multiple) {
+        this.moveColorToLibrary().then(() => {
+          $wx.navigateBack(2)
+        }, {}, '已移动到 ' + this.data.name)
+      } else if (this.props.type === constant.ColorLibraryActionType.Add_Single
+        || this.props.type === constant.ColorLibraryActionType.Add_Multiple
+        || this.props.type === constant.ColorLibraryActionType.SaveColor
+        ) {
+        this.addColorToLibrary().then(() => {
+          $wx.navigateBack(2, {}, '已加入到 ' + this.data.name)
+        })
+      } else {
+        $wx.navigateBack()
+      }
+    })
   },
   nameChange: function (d, v) {
-    
     this.setData({
       name: v
     })
-
     this.checkCanSave()
-
   },
   descChange: function (d, v) {
-
     this.setData({
       desc: v
     })
-
-    this.checkCanSave()
-
   },
 }
 
@@ -117,20 +86,14 @@ const privateMethods = {
     return !!str && str.length > 0
   },
   checkCanSave: function () {
-    if (this.isNotEmpty(this.data.name)) {
-      this.setData({
-        canSave: true
-      })
-    } else {
-      this.setData({
-        canSave: false
-      })
-    }
+    this.setData({
+      canSave: this.isNotEmpty(this.data.name)
+    })
   },
   createColorLibrary: function () {
     return http.post(urls.colorLibrarySave, {
       // mock: true,
-      id: this.data.id,
+      id: this.data.libraryId,
       name: this.data.name,
       description: this.data.desc
     })
@@ -139,14 +102,14 @@ const privateMethods = {
     return http.post(urls.addColor, {
       // mock: true,
       libraryId: this.data.libraryId,
-      libraryColorIdList: this.data.libraryColorIdList,
+      libraryColorIdList: this.props.libraryColorIdList,
     })
   },
   moveColorToLibrary: function () {
     return http.post(urls.moveColor, {
       // mock: true,
       libraryId: this.data.libraryId,
-      libraryColorIdList: this.data.libraryColorIdList,
+      libraryColorIdList: this.props.libraryColorIdList,
     })
   },
 }
