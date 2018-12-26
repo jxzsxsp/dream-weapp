e0b2746c399653548b065c8b0ab0d5e19f8dad7c
zhang759740844 <759740844@qq.com>
2018-12-26 17:21:40 +0800

    fix 非首页搜索

--- pages/color-list/color-list.js
+++ pages/color-list/color-list.js
@@ -10,18 +10,6 @@ let data = {
   colorList: {}
 }
 
-let privateMethod = {
-  onReachBottom () {
-    http.getPantoneList(urls.pantone.colorSearch, this.props.loadingState)
-      .then(colorList => {
-        this.data.colorList.push(...colorList)
-        this.setData({
-          colorList: this.data.colorList
-        })
-      })
-  }
-}
-
 let lifeCycle = {
   onLoad: function (colorDetail) {
     this.props.categoryId = colorDetail.id
@@ -34,26 +22,67 @@ let lifeCycle = {
           colorList
         })
       }) 
+  },
+  onReachBottom () {
+    this.searchColor(true)
   }
 }
 
 let viewAction = {
+  onInput: function (data, value) {
+    console.log(value)
+    this.setKeyword(value)
+    this.clearTimeoutSearch()
+    this.setTimeoutSearch()
+  },
   beginSearch: function (data, value) {
-    if (typeof(value) === 'object') {
-      value = ''
-    }
-    this.props.loadingState = http.defaultLoadingState()
-    http.getPantoneList(urls.pantone.colorSearch, this.props.loadingState, {keyword: value, categoryId: this.props.categoryId})
-      .then((colorList) => {
-        this.setData({
-          colorList
-        })
-      })
+    this.setKeyword(value)
+    this.searchColor(false)
   },
 
   colorClicked: function (data) {
     $wx.navigateTo($wx.router.colorDetail, {colorId: this.data.colorList[data.index].colorId})
   }
+}
+
+const privateMethod = {
+  setTimeoutSearch: function () {
+    this.props.timeout = setTimeout(() => {
+      this.searchColor(false)
+    }, 500)
+  },
+  clearTimeoutSearch: function () {
+    if (this.props.timeout) {
+      clearTimeout(this.props.timeout)
+      this.props.timeout = null
+    }
+  },
+  setKeyword: function (value) {
+    if (typeof(value) === 'object') {
+      value = ''
+    } 
+    this.props.keyword = value
+  },
+  /* 搜索颜色方法，addMore 表示搜索后面页面还是重新搜索第一页 */
+  searchColor: function (addMore) {
+    if (!this.props.keyword) {
+      return
+    }
+    if (!addMore) {
+      this.props.loadingState = http.defaultLoadingState()
+    }
+    http.getPantoneList(urls.pantone.colorSearch, this.props.loadingState, {keyword: this.props.keyword}).then((res) => {
+        if (addMore) {
+          this.data.colorList.push(...res)
+        } else {
+          this.data.colorList = res
+        }
+        this.setData({
+          colorList: this.data.colorList,
+          isSearching: true
+        })
+      }) 
+  },
 }
 
 $Page.register(props, data, lifeCycle, privateMethod, viewAction)
