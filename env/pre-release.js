9dba7d9a7f89187659daaea76c318e76ce729768
zhang759740844 <759740844@qq.com>
2018-12-27 10:25:23 +0800

    add 预发环境

---
+++ env/pre-release.js
@@ -0,0 +1,7 @@
+export const isOnline = false
+
+export default {
+  Login: 'https://sso2.lian-shang.cn',
+  url: 'https://chameleon.lian-shang.cn',  //主域名
+  Pantone: 'https://colorlab.lian-shang.cn'
+}
