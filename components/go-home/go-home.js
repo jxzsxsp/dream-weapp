c4df43ca56e62ca47b1a798aa583e0d3599bdbe5
Sean.Xiao <jxzsxsp@qq.com>
2018-12-28 18:13:52 +0800

    1、添加通用悬浮组件；2、添加回首页组件；3、优化色差胶囊样式问题；

---
+++ components/go-home/go-home.js
@@ -0,0 +1,26 @@
+// components/go-home/go-home.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    goHome: function () {
+      wx.reLaunch({ url: '/pages/index/index' })
+    },
+  }
+})
