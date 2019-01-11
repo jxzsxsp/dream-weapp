54abfa1a09ecdb518316a79ddeeb8479a7f5e44a
maxine <422789482@qq.com>
2019-01-11 20:10:05 +0800

    一阵专区抢光增加图标

--- pages/screening/screening.js
+++ pages/screening/screening.js
@@ -7,6 +7,7 @@ Page({
      * 页面的初始数据
      */
     data: {
+        nullDraw: app.getRequestUrl + "/Templates/xcxshop/images/null.png",
         isHotState:true,
         nothing:0,
         isSoldOut:0,
