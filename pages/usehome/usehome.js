346da45635981d234682226dc12173ae95959917
潘金龙 <992477019@qq.com>
2019-01-13 13:45:11 +0800

    userhome 关注入口

--- pages/usehome/usehome.js
+++ pages/usehome/usehome.js
@@ -73,6 +73,7 @@ Page({
     bindAtention: function () {
       wx.navigateTo({
         url: "/pages/myAtention/myAtention"
+        //   url: "/pages/myFocus/myFocus"
       });
     },
     bindApply: function(o) {
