f2c00af45746d95e2c033ec95b5f4c2a67068ee6
Sean.Xiao <jxzsxsp@qq.com>
2019-01-14 18:36:06 +0800

    完成个人中心UI及交互

--- pages/user-center/user-center.js
+++ pages/user-center/user-center.js
@@ -2,9 +2,12 @@ import {$Page, $wx} from '../../genji4mp/index'
 
 const data = {
   settingList: [
-    { name: '我的收藏', icon: 'icon-xing' },
+    { name: '关注店铺', icon: 'icon-shoucang', half: true },
+    { name: '最近浏览', icon: 'icon-ic_history', half: true },
+    { name: '我的收藏', icon: 'icon-shoucang', half: false },
   ],
   userInfo: {},
+  showShareImg: false,
 }
 
 const privateMethod = {
@@ -20,18 +23,45 @@ const lifecycle = {
         userInfo: $wx.app.globalData.userInfo
       })
     }
-  }
+  },
+  onShareAppMessage: function () {
+    return {
+      title: `小蜥取色`,
+      path: `/pages/index/index`,
+      imageUrl: 'http://img50.lianshang.cn/data/share.jpg'
+    }
+  },
 }
 
 const viewAction = {
   itemClicked (d, v) {
     if (d.index === 0) {
+      // 关注店铺
+      $wx.navigateTo($wx.router.followShop)
+    } else if (d.index === 1) {
+      // 最近浏览 
+      $wx.navigateTo($wx.router.latestView)
+    } else if (d.index === 2) {
       // 我的收藏
       $wx.navigateTo($wx.router.favoriteLibrary)
-    } else if (d.index === 1) {
-      // 设备管理 
-      $wx.navigateTo($wx.router.deviceManager)
     }
+  },
+  callContact: function() {
+    $wx.makePhoneCall({
+      phoneNumber: '02161161196',
+    })
+  },
+  showShareImg: function(d, v) {
+    this.setData({
+      showShareImg: true
+    })
+  },
+  closeShareImg: function (d, v) {
+    this.setData({
+      showShareImg: false
+    })
+  },
+  saveImg: function() {
   },
   getUserInfo () {
     $wx.app.bindPhone().then(res => {
