4ac817343741439518484f43f0d6373e9c9770e3
潘金龙 <992477019@qq.com>
2019-02-20 16:20:35 +0800

    佣金

--- pages/Commission/Commission.js
+++ pages/Commission/Commission.js
@@ -1,156 +1,65 @@
-// pages/Commission/Commission.js
 var t = require("../../utils/config.js"),
     a = getApp();
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
         userInfo: {},
         openId: "",
         PageIndex: 1,
         PageSize: 10,
-        SplittinList: null,
+        SplittinList: [],
         isempty: !0,
         SplittinTotal: "",
         CanDrawSplittin: "",
         NoSettlementSplttin: "",
         DrawSplittinTotal: "",
         isDefault: true,
-        DistributionInfo: ""
+        DistributionInfo: "",
+        hasMore: true,
+        dataIndex: 0
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function(options) {
         var tm = this;
-        a.getUserInfo(function (t) {
+        a.getUserInfo(function(t) {
             tm.setData({
                 userInfo: t
             })
         });
-        a.getOpenId(function (t) {
+        a.getOpenId(function(t) {
             wx.request({
                 url: a.getUrl("GetReferralInfo"),
                 data: {
                     openId: t
+                    // openId: "o_rWK5f-5hvhMrySuLW5L7d_vTwA"
                 },
-                success: function (t) {
+                success: function(t) {
                     a.globalData.ReferralInfo = t.data.referral_get_response, tm.GetCheckData();
                 }
             });
         });
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
+    onShow: function() {
+        var tm = this;
+        tm.loadMore()
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-        var t = this;
-        t.setData({
-            PageIndex: 1
-        }), t.loadData(t, !1);
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
     onPullDownRefresh: function() {
         var t = this;
-        t.loadData(t, !1);
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-        var t = this,
-            a = t.data.PageIndex + 1;
         t.setData({
-            PageIndex: a
-        }), t.loadData(t, !0);
+            dataIndex: 0,
+            hasMore: true
+        })
+        t.loadMore();
     },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
+    onReachBottom: function() {
+        var t = this;
+        t.data.hasMore ? t.loadMore() : ""
     },
-    loadData: function (i, n) {
-        wx.showLoading({
-            title: "加载中"
-        }), a.getOpenId(function (e) {
-            wx.request({
-                url: a.getUrl("SplittinList"),
-                data: {
-                    openId: e,
-                    pageIndex: i.data.PageIndex,
-                    pageSize: i.data.PageSize
-                },
-                success: function (a) {
-                    if (void 0 == a.data.error_response) {
-                        var e = a.data.splittin_get_response,
-                            l = e.SplittinList;
-                        if (n) {
-                            var o = i.data.SplittinData;
-                            o.push.apply(o, l), i.setData({
-                                SplittinList: o,
-                                SplittinTotal: i.data.SplittinTotal,
-                                CanDrawSplittin: i.data.CanDrawSplittin,
-                                NoSettlementSplttin: i.data.NoSettlementSplttin,
-                                DrawSplittinTotal: i.data.DrawSplittinTotal
-                            });
-                        } else {
-                            l.Total;
-                            i.setData({
-                                SplittinList: l,
-                                isEmpty: i.data.isempty,
-                                SplittinTotal: e.SplittinTotal,
-                                CanDrawSplittin: e.CanDrawSplittin,
-                                NoSettlementSplttin: e.NoSettlementSplttin,
-                                DrawSplittinTotal: e.DrawSplittinTotal
-                            });
-                        }
-                    } else t.showTip(a.data.error_response.errMsg);
-                },
-                complete: function () {
-                    wx.hideLoading();
-                }
-            });
-        });
-    },
-
-    bindSplittinDraw: function (t) {
+    bindSplittinDraw: function(t) {
         var tm = this;
         if (tm.data.DistributionInfo.ReferralGradeName == '试用' || !tm.data.DistributionInfo.ReferralGradeName) {
             wx.showModal({
                 title: '',
                 content: '您是试用VIP，不可提现，请开通永久VIP',
-                success: function (res) {
+                success: function(res) {
                     if (res.confirm) { //这里是点击了确定以后
                         wx.navigateTo({
                             url: "../longvip/longvip"
@@ -166,25 +75,60 @@ Page({
             });
         }
     },
-    changeList: function (e) {
+    changeList: function(e) {
         var tm = this;
         if (e.currentTarget.dataset.flag === tm.data.isDefault) return;
         tm.setData({
             isDefault: !tm.data.isDefault
         })
     },
-    GetCheckData: function () {
+    GetCheckData: function() {
         this.setData({
             DistributionInfo: a.globalData.ReferralInfo
         });
 
     },
-    GoArticle:function(e){
+    GoArticle: function(e) {
         var url = "https://ytal.qkmai.com/vShop/ArticleDetails?ArticleId=" + e.currentTarget.dataset.id
         var deurl = encodeURIComponent(url)
         var s = '/pages/webPage/webPage?artUrl=' + deurl
         wx.navigateTo({
             url: s
         })
+    },
+    loadMore: function() {
+        var tm = this;
+        wx.showLoading({
+            title: "加载中"
+        });
+        wx.request({
+            url: a.getUrl("SplittinList"),
+            data: {
+                // openId: tm.data.userInfo.OpenId,
+                openId: "o_rWK5f-5hvhMrySuLW5L7d_vTwA",
+                pageIndex: ++tm.data.dataIndex,
+                pageSize: tm.data.PageSize
+            },
+            success: function(res) {
+                if (res.data.splittin_get_response.SplittinList.length != 10) {
+                    tm.setData({
+                        hasMore: false
+                    })
+                }
+                var oldList = tm.data.SplittinList
+                var newList = oldList.concat(res.data.splittin_get_response.SplittinList)
+                tm.setData({
+                    SplittinList: newList,
+                    SplittinTotal: res.data.splittin_get_response.SplittinTotal,
+                    CanDrawSplittin: res.data.splittin_get_response.CanDrawSplittin,
+                    NoSettlementSplttin: res.data.splittin_get_response.NoSettlementSplttin,
+                    DrawSplittinTotal: res.data.splittin_get_response.DrawSplittinTotal
+                })
+                wx.hideLoading();
+            },
+            complete: function() {
+                wx.hideLoading();
+            }
+        });
     }
 })
